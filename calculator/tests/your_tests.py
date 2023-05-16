3a5cc82584816b6450bbcea5b34cd26e8256e12a
santiramos <elsantinoramos@gmail.com>
2023-05-15 23:45:32 -0700

    compiled calculator executable with makefile and added CI Tests

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 from calculator_adapter import run
 
-
-### ADD AT LEAST TWO TESTS HERE!
-
-
-###
-
+# Checks that the program outputs "2" for an input of "3 - 1"
+assert run("3 - 1").output == "2"
+# Checks that the program outputs "3" for an input of "6 / 2"
+assert run("6 / 2").output == "3"
+# Checks that the program fails (correctly errors) for input "5 $ 5"
+assert run("5 $ 5").exit_status != 0
+  
 print("All tests passed!")
